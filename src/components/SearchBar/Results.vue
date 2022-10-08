632b27903fb2dd88e738c44b4abead06d8a35ed3
Johan Santana <johancarlosst@gmail.com>
2022-10-07 22:32:24 -0400

    feat: created Results.vue and restyled index

---
+++ src/components/SearchBar/Results.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  search: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <div class="bg-white p-5 rounded-b-lg min-w-full border-t-2 font-light border-gray-100">
+    <p>This is the result component</p>
+    <p>Here will appear pokemons based on your input search</p>
+    <br />
+    <p
+      >Your actual <span class="underline decoration-2 decoration-emerald-700/30">search</span> is: <br />{{
+        search
+      }}</p
+    >
+  </div>
+</template>
--- src/components/SearchBar/index.vue
+++ src/components/SearchBar/index.vue
@@ -1,22 +1,27 @@
 <script setup>
 import { onMounted, ref, watchEffect } from 'vue'
+import Results from './Results.vue'
 
 const isOpen = ref(false)
 const searchInput = ref(null)
-
+const hasResults = ref(true)
+const search = ref(null)
 watchEffect(() => {
   if (searchInput.value) searchInput.value.focus()
 })
 
 onMounted(() => {
-  window.addEventListener('keydown', evt => {
-    if (evt.code == 'KeyK' && evt.ctrlKey) {
-      evt.preventDefault()
+  window.addEventListener('keydown', e => {
+    if (e.code === 'KeyK' && e.ctrlKey) {
+      e.preventDefault()
+      isOpen.value = !isOpen.value
+    }
+    if (e.code === 'Escape' && isOpen.value) {
       isOpen.value = !isOpen.value
     }
   })
-  window.addEventListener('click', evt => {
-    if (evt.target === searchInput.value) return
+  window.addEventListener('click', e => {
+    if (e.target === searchInput.value) return
     if (!isOpen.value) return
     isOpen.value = !isOpen.value
   })
@@ -24,18 +29,25 @@ onMounted(() => {
 </script>
 
 <template>
-  <Teleport to="body">
+  <Teleport to="#search">
     <div
       v-if="isOpen"
-      class="min-h-screen min-w-full flex items-center justify-center absolute top-0 backdrop-blur-sm backdrop-brightness-95"
+      class="min-h-screen min-w-full flex items-center justify-center absolute top-0 backdrop-blur-sm backdrop-brightness-95 p-4"
     >
-      <input
-        ref="searchInput"
-        type="text"
-        placeholder="Write some pokemon name..."
-        class="input input-bordered bg-white w-full -translate-y-60 max-w-md input-lg select-none"
-      />
-      <div> </div>
+      <div class="min-w-full flex items-center flex-col justify-center relative -translate-y-60">
+        <input
+          ref="searchInput"
+          type="text"
+          placeholder="Write some pokemon name..."
+          v-model="search"
+          class="input lg:min-w-[500px] border-b-none bg-white w-full max-w-md input-lg select-none focus:outline-none"
+          :class="{ 'rounded-b-none': hasResults }"
+        />
+        <div class="min-w-[500px]">
+          <!-- Results -->
+          <Results :search="search" />
+        </div>
+      </div>
     </div>
   </Teleport>
 </template>
