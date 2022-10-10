c0e2e20bbb0bc0fe10ddc1f0f03a7c98116a6fb8
Johan Santana <johancarlosst@gmail.com>
2022-10-09 22:00:36 -0400

    feat: add mobile support to searchbar

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <script setup>
+import { useBreakpoints, breakpointsTailwind } from '@vueuse/core'
 import SearchBar from './components/SearchBar/index.vue'
 import PokemonList from './components/Pokemon/PokemonList.vue'
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const sm = breakpoints.smallerOrEqual('sm')
 
 const IMAGE_URL =
   'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/121.png'
 </script>
 
 <template>
-  <SearchBar />
-  <div class="min-h-screen bg-white p-14">
-    <h1 class="mx-auto w-fit text-4xl font-medium mb-10">Pokémon Search</h1>
-    <div class="w-fit mx-auto mb-20">
-      <div class="mb-4 flex items-center text-xl font-light">
-        <span class="mr-4">Press</span>
-        <kbd class="kbd bg-transparent mr-4">Ctrl</kbd> +
-        <kbd class="kbd mx-4 bg-transparent">K</kbd>
-        <span>to make a search</span>
-      </div>
-      <div class="flex justify-center font-light text-lg">
-        <p>Or browse the list</p>
-      </div>
+  <main class="min-h-screen bg-white p-6 md:p-10 lg:p-14">
+    <SearchBar v-if="sm" />
+    <div v-else>
+      <Teleport to="#search">
+        <SearchBar />
+      </Teleport>
+      <!-- Main content -->
+      <section>
+        <h1 class="mx-auto w-fit text-4xl font-medium mb-10">Pokémon Search</h1>
+        <div class="w-fit mx-auto mb-20">
+          <div class="mb-4 flex items-center text-xl font-light">
+            <span class="mr-4">Press</span>
+            <kbd class="kbd bg-transparent mr-4">Ctrl</kbd> +
+            <kbd class="kbd mx-4 bg-transparent">K</kbd>
+            <span>to make a search</span>
+          </div>
+          <div class="flex justify-center font-light text-lg">
+            <p>Or browse the list</p>
+          </div>
+        </div>
+      </section>
     </div>
-    <h2 class="text-4xl font-medium mb-6">Pokémon List</h2>
-    <main>
+    <section>
+      <h2 class="text-4xl font-medium mt-10 mb-6">Pokémon List</h2>
       <PokemonList />
-    </main>
-  </div>
+    </section>
+  </main>
 </template>
--- src/components/SearchBar/index.vue
+++ src/components/SearchBar/index.vue
@@ -1,57 +1,87 @@
 <script setup>
-import { onMounted, ref, watchEffect } from 'vue'
-import { useMagicKeys, onClickOutside, whenever } from '@vueuse/core'
+import { ref, watchEffect } from 'vue'
+import { useMagicKeys, onClickOutside, whenever, useBreakpoints, breakpointsTailwind } from '@vueuse/core'
 import Results from './Results.vue'
+
+const props = defineProps({
+  isOpen: {
+    type: Boolean
+  }
+})
 
 const isOpen = ref(false)
 const inputRef = ref(null)
-const hasResults = ref(true)
+const hasResults = ref(false)
 const search = ref(null)
 
-const { ctrl_k, escape } = useMagicKeys({
-  passive: false,
-  onEventFired(e) {
-    if (e.ctrlKey && e.key === 'k' && e.type === 'keydown') e.preventDefault()
-    if (e.Escape && e.type === 'keydown') e.preventDefault()
-  }
-})
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const sm = breakpoints.smallerOrEqual('sm')
 
-whenever(ctrl_k, () => {
-  isOpen.value = !isOpen.value
-})
-whenever(escape, () => (isOpen.value = !isOpen.value))
 watchEffect(() => {
-  if (inputRef.value) {
-    search.value = null
-    inputRef.value.focus()
-  }
+  if (sm.value) isOpen.value = true
+  else isOpen.value = false
 })
 
+watchEffect(() => {
+  if (search.value) hasResults.value = true
+})
+
+if (!sm.value) {
+  const { ctrl_k, escape } = useMagicKeys({
+    passive: false,
+    onEventFired(e) {
+      if (e.ctrlKey && e.key === 'k' && e.type === 'keydown') e.preventDefault()
+      if (e.Escape && e.type === 'keydown') e.preventDefault()
+    }
+  })
+
+  whenever(ctrl_k, () => {
+    isOpen.value = !isOpen.value
+  })
+  whenever(escape, () => (isOpen.value = !isOpen.value))
+  watchEffect(() => {
+    if (inputRef.value) {
+      search.value = null
+      inputRef.value.focus()
+    }
+  })
+}
 onClickOutside(inputRef, e => {
+  if (sm.value) {
+    hasResults.value = false
+    return
+  }
   isOpen.value = !isOpen.value
 })
 </script>
 
 <template>
-  <Teleport to="#search">
+  <div
+    v-if="isOpen"
+    class="flex min-w-full items-center justify-center top-0"
+    :class="{ 'min-h-screen backdrop-blur-sm backdrop-brightness-95 absolute p-4': !sm }"
+  >
     <div
-      v-if="isOpen"
-      class="min-h-screen min-w-full flex items-center justify-center absolute top-0 backdrop-blur-sm backdrop-brightness-95 p-4"
+      class="min-w-[500px] flex items-center flex-col justify-center relative"
+      :class="{ '-translate-y-60': !sm }"
     >
-      <div class="min-w-full flex items-center flex-col justify-center relative -translate-y-60">
-        <input
-          ref="inputRef"
-          type="text"
-          placeholder="Write some pokémon name..."
-          v-model="search"
-          class="input lg:min-w-[500px] border-b-none bg-white w-full max-w-md input-lg select-none focus:outline-none"
-          :class="{ 'rounded-b-none': hasResults }"
+      <input
+        ref="inputRef"
+        type="text"
+        placeholder="Write some pokémon name..."
+        v-model="search"
+        class="input lg:min-w-[500px] border-b-none bg-white min-w-full max-w-md input-lg select-none focus:outline-none"
+        :class="{ 'rounded-b-none': hasResults, 'input-bordered': sm }"
+      />
+      <div class="lg:min-w-[500px]">
+        <!-- Results -->
+        <Results
+          v-if="hasResults"
+          :search="search"
+          class="min-w-[500px]"
+          :class="{ 'border absolute left-0': sm }"
         />
-        <div class="lg:min-w-[500px]">
-          <!-- Results -->
-          <Results :search="search" />
-        </div>
       </div>
     </div>
-  </Teleport>
+  </div>
 </template>
