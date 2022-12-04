cf3a6a3ef35a6b08f356c2a1c586a22bf5ced18c
Johan Santana <johancarlosst@gmail.com>
2022-12-03 20:52:13 -0400

    refactor: imported vue native capitalize and pokemon interface

--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -1,22 +1,33 @@
 import { API } from '.'
+import { capitalize } from 'vue'
+import { Pokemon } from '../ts/interfaces/pokemon.interface'
 
 export const getAllPokemons = async () => {
   try {
     const result = await fetch(`${API}/pokemon/`)
-    const data = await result.json()
-    return data
+    const pokemonArray = await result.json()
+    return pokemonArray
   } catch (err) {
     throw new Error(err)
   }
 }
 
-export const getPokemonByUrl = async (url: string, options: Object) => {
+/**
+ * Fetches a pokemon by its url
+ * @param url The fetch url with pokemon id e.g "https://pokeapi.co/api/v2/pokemon/1"
+ * @returns The pokemon data specified in the url
+ */
+export const getPokemonByUrl = async (url: string): Promise<Pokemon> => {
   try {
     if (!url) throw new Error('No Pokemon Url has been defined')
-    const result = await fetch(url, options ? options : {})
-    const data = await result.json()
-    return data
+
+    const result = await fetch(url)
+    const pokemonData = await result.json()
+    pokemonData.name = capitalize(pokemonData.name)
+    return pokemonData
   } catch (err) {
     throw new Error(err)
   }
 }
+
+export const getPokemonByName = async (name: string) => {}
