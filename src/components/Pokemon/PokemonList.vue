23145b401e701c557b67e3387fe1b62bb94dabe4
Johan Santana <johancarlosst@gmail.com>
2022-12-05 00:23:48 -0400

    feat: import FilterList

--- src/components/Pokemon/PokemonList.vue
+++ src/components/Pokemon/PokemonList.vue
@@ -3,6 +3,7 @@ import { ref, onBeforeMount } from 'vue'
 import { getAllPokemons } from '../../api/pokemon'
 import PokemonCard from './PokemonCard.vue'
 import AppPagination from '../AppPagination.vue'
+import FilterList from '../FilterList/FilterList.vue'
 
 const pokemonItemsList = ref([])
 
@@ -13,16 +14,19 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <div
-    class="grid rounded-3xl gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 my-10"
-  >
-    <PokemonCard
-      v-for="pokemon in pokemonItemsList"
-      :key="pokemon.id"
-      :pokemonUrl="pokemon.url"
-    />
-  </div>
-  <div class="flex justify-center">
-    <AppPagination class="w-1/2" />
+  <div>
+    <FilterList />
+    <div
+      class="grid rounded-3xl gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 my-10"
+    >
+      <PokemonCard
+        v-for="pokemon in pokemonItemsList"
+        :key="pokemon.name"
+        :pokemonUrl="pokemon.url"
+      />
+    </div>
+    <div class="flex justify-center">
+      <AppPagination class="w-1/2" />
+    </div>
   </div>
 </template>
