f9ce996bdd3b667d86030eb48452f860a7761e60
Johan Santana <johancarlosst@gmail.com>
2022-12-05 00:23:19 -0400

    feat: add pagination interactivity

--- src/components/AppPagination.vue
+++ src/components/AppPagination.vue
@@ -4,19 +4,27 @@ import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/vue/20/solid'
 
 const currentPage = ref(1)
 
-const goForwards = () => {}
-const goBackwards = () => {}
+const showNext = () => {
+  currentPage.value++
+}
+const showPrevious = () => {
+  currentPage.value--
+}
 </script>
 
 <template>
   <div class="flex items-center justify-center gap-4">
-    <button class="btn border-slate-300" v-if="currentPage > 1">
+    <button
+      class="btn border-slate-300"
+      v-if="currentPage > 1"
+      @click="showPrevious"
+    >
       <ChevronLeftIcon class="w-5 h-5" />
     </button>
     <span class="font-medium text-slate-400 text-lg">
       Page {{ currentPage }}
     </span>
-    <button class="btn border-slate-300">
+    <button class="btn border-slate-300" @click="showNext">
       <ChevronRightIcon class="w-5 h-5" />
     </button>
   </div>
