08a2e70ed5b585dfc6c8f3b9bf28fe18230e86ce
Johan Santana <johancarlosst@gmail.com>
2022-12-05 00:22:14 -0400

    feat: added filter List components

---
+++ src/components/FilterList/FilterList.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+import FilterOptions from './FilterOptions.vue'
+import { ChevronDownIcon } from '@heroicons/vue/20/solid'
+import { onClickOutside } from '@vueuse/core'
+
+const isShowingOptions = ref(false)
+
+const toggleShow = () => {
+  isShowingOptions.value = !isShowingOptions.value
+}
+
+const filterReference = ref(null)
+
+onClickOutside(filterReference, e => {
+  if (e.target.matches('#filter-btn') || e.target.matches('#filter-btn *'))
+    return
+  if (isShowingOptions.value) {
+    isShowingOptions.value = false
+  }
+})
+</script>
+
+<template>
+  <div>
+    <p class="mb-3 text-lg text-slate-500 italic">Filter by:</p>
+    <div class="border-2 p-2 rounded-full relative">
+      <button
+        class="btn btn-primary p-1 px-3 rounded-full text-yellow-600"
+        id="filter-btn"
+        @click="toggleShow"
+      >
+        <span>Type</span>
+        <ChevronDownIcon
+          class="w-5 h-5 transition-transform"
+          :class="{ 'rotate-180': isShowingOptions }"
+        />
+      </button>
+
+      <FilterOptions
+        class="absolute mt-1"
+        v-if="isShowingOptions"
+        ref="filterReference"
+      />
+    </div>
+  </div>
+</template>
---
+++ src/components/FilterList/FilterOptions.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="bg-base-100 border rounded-3xl p-4 text-gray-400">
+    Here will appear filter options
+  </div>
+</template>
