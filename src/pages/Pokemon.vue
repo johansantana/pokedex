d959e9f5604688a509142fdd55a7e81dbc48b6e8
Johan Santana <johancarlosst@gmail.com>
2022-12-09 20:37:26 -0400

    fix: router and set description prop

--- src/pages/Pokemon.vue
+++ src/pages/Pokemon.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { onBeforeMount, ref, capitalize } from 'vue'
+import { onBeforeMount, ref, capitalize, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import { getAllPokemons, getPokemon } from '../utils'
+import { getAllPokemons, getPokemon, getPokemonDescription } from '../utils'
 import AppButton from '../components/AppButton.vue'
 import PokemonInfo from '../components/Pokemon/PokemonInfo.vue'
 import { ArrowUturnLeftIcon } from '@heroicons/vue/20/solid'
@@ -10,6 +10,7 @@ const route = useRoute()
 const router = useRouter()
 
 const pokemon = ref(null)
+const pokemonDescription = ref('')
 
 onBeforeMount(async () => {
   const pokemons = await getAllPokemons()
@@ -19,6 +20,7 @@ onBeforeMount(async () => {
 
   if (!pokemonFound) router.push('/' + route.params.name)
   pokemon.value = await getPokemon(pokemonFound.id)
+  pokemonDescription.value = await getPokemonDescription(pokemonFound.id)
 })
 </script>
 
@@ -27,16 +29,21 @@ onBeforeMount(async () => {
     <div
       class="flex flex-col gap-10 items-start md:flex-row md:justify-between"
     >
-      <AppButton class="px-4 grow-0" @click="$router.push('/')">
+      <AppButton class="px-4 grow-0" @click="$router.back()">
         <ArrowUturnLeftIcon class="w-4 h-4" />
         <span>Go Back</span>
       </AppButton>
       <h1
         class="text-7xl sm:text-8xl w-fit font-bold text-transparent bg-clip-text bg-gradient-to-r from-slate-800 via-slate-600 to-blue-400"
       >
-        {{ capitalize(pokemon.name) }}
+        {{
+          pokemon.name
+            ?.split('-')
+            ?.map(word => capitalize(word))
+            ?.join(' ')
+        }}
       </h1>
     </div>
-    <PokemonInfo :pokemon="pokemon" />
+    <PokemonInfo :pokemon="pokemon" :description="pokemonDescription" />
   </div>
 </template>
